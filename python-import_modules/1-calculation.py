841bab8dde6d71953231f22e153f16df501c713e
hesen <hesen123@gmail.com>
2026-06-01 15:00:30 +0400

    ADASD

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-from calculator_1 import add, subtract, multiply, divide
+from calculator_1 import add, subtract as sub, multiply as mul, divide as div
 a = 10
 b = 5
 if __name__ == "__main__":
     print("{} + {} = {}".format(a, b, add(a, b)))
-    print("{} - {} = {}".format(a, b, subtract(a, b)))
-    print("{} * {} = {}".format(a, b, multiply(a, b)))
-    print("{} / {} = {}".format(a, b, divide(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
